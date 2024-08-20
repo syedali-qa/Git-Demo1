ae16a87d456f61e1396bd5d386ff97225c52c247
syedali-qa <syedalitest1@gmail.com>
2024-08-20 19:05:53 +0530

    added sub and mul to sample java class

--- src/test/java/stepdefintions/SampleJava.java
+++ src/test/java/stepdefintions/SampleJava.java
@@ -5,4 +5,12 @@ public class SampleJava {
 	public int sum(int a, int b) {
 		return a + b;
 	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	
+	public int mul(int a,int b) {
+		return a*b;
+	}
 }
